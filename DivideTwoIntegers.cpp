38754a761af7badd1961e3336d0c961cdcf02630
Shaobin Xu <4everlove.xu@gmail.com>
2014-12-03 01:33:10 -0500

    use another method to subtract divisor exponetially

--- DivideTwoIntegers.cpp
+++ DivideTwoIntegers.cpp
@@ -29,15 +29,45 @@ public:
     }
 };
 
+class Solution1 {
+public:
+    int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            throw;
+
+        int sign = 1;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            sign = -1;
+        // In case INT_MIN overflow
+        long long N = dividend >= 0 ? dividend : -(long long)dividend;
+        long long D = divisor >= 0 ? divisor : -(long long)divisor;
+
+        int quotient = 0, i = 0;
+        
+        while (N >= D) {
+            long long tmp = D;
+            for (int i = 0; N >= tmp; ++i, tmp <<= 1) {
+                N -= tmp;
+                quotient += 1 << i;
+            }
+        }
+
+        if (sign > 0) return quotient;
+        else  return -quotient;
+    }
+};
+
 int main()
 {
-    Solution solver;
+    Solution1 solver;
     std::cout << "14 / 7: " << (solver.divide(14, 7) == 14 / 7 ? "CHECK" : "WRONG") << std::endl
-              << "26 / 3: " << (solver.divide(26, 3) == 26 / 3 ? "CHECK" : "WRONG")<< std::endl
+              << "26 / 3: " << (solver.divide(26, 3) == 26 / 3 ? "CHECK" : "WRONG") << std::endl
               << "-14 / -7: " << (solver.divide(-14, -7) == -14 / -7 ? "CHECK" : "WRONG") << std::endl
               << "-26 / -3: " << (solver.divide(-26, -3) == -26 / -3 ? "CHECK" : "WRONG") << std::endl
               << "24 / -7: " << (solver.divide(24, -7) == 24 / -7 ? "CHECK" : "WRONG") << std::endl
               << "1 / -1: " << (solver.divide(1, -1) == 1 / -1 ? "CHECK" : "WRONG") << std::endl
               << "-24 / 7: " << (solver.divide(-24, 7)  == -24 / 7 ? "CHECK" : "WRONG") << std::endl
-              << "2147483647 / 1: " << (solver.divide(2147483647, 1) == 2147483647 / 1 ? "CHECK" : "WRONG")<< std::endl;
+              << "2147483647 / 1: " << (solver.divide(2147483647, 1) == 2147483647 / 1 ? "CHECK" : "WRONG") << std::endl
+              << "1004958205 / -2137325331: " << (solver.divide(1004958205, -2137325331) == 1004958205 / -2137325331 ? "CHECK" : "WRONG") << std::endl
+              << "-2147483648 / 1: " << (solver.divide(-2147483648, 1) == -2147483648 / 1 ? "CHECK" : "WRONG") << std::endl;
 }
